25608c769166cef3473d39b7c69a5e2e6130a2d2
juanaorcajo <orcajojuana@gmail.com>
2025-11-15 13:56:10 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,33 @@
+let cursorImg;
+let juego;
+let fondoInicio;
+let fondoFinal;
+
+//https://youtu.be/q9RQigoz_2M
+
+function preload() {
+  cursorImg = loadImage("data/cursor.png");
+  fondoInicio = loadImage("data/fondoInicio.png"); // fondo instrucciones
+  fondoFinal = loadImage("data/fondoFinal.png");   // fondo créditos
+  Sonido.preload();
+}
+
+
+function setup() {
+  createCanvas(640, 480);
+  noCursor();
+  juego = new Juego();
+}
+
+function draw() {
+  background(0);
+  juego.mostrar();
+
+  // Cursor personalizado
+  imageMode(CENTER);
+  image(cursorImg, mouseX, mouseY, 70, 70);
+}
+
+function mousePressed() {
+  juego.click(mouseX, mouseY);
+}
